673b7d365951b86218e40d0384ce4408962c02af
xuhuidong <HuiD_Xu@163.com>
2022-06-10 10:12:07 -0700

    find bug: bytes not write into the reassembler

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,35 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    // set the initial sequence number if necessary
+    if (seg.header().syn) {
+        _init_seqno.emplace(seg.header().seqno);
+        _syn_received = true;
+        _fin_received = false;
+    };
+    if (seg.header().fin) {
+        _fin_received = true;
+        _next_ackno.reset();
+    };
+
+    if (_syn_received) {
+        const std::string data = seg.payload().copy();
+        const uint64_t index = unwrap(seg.header().seqno, _init_seqno.value(), 0);
+        const bool eof = seg.header().fin;
+
+        _reassembler.push_substring(data, index, eof);
+
+        if (_next_ackno.has_value())
+            _next_ackno.emplace(_next_ackno.value() + seg.payload().size() + seg.header().syn + seg.header().fin);
+        else
+            _next_ackno.emplace(_init_seqno.value() + seg.payload().size() + seg.header().syn + seg.header().fin);
+    };
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    return _next_ackno;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return _capacity - _reassembler.stream_out().buffer_size();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -18,14 +18,19 @@ class TCPReceiver {
     StreamReassembler _reassembler;
 
     //! The maximum number of bytes we'll store.
-    size_t _capacity;
+    size_t _capacity{};
+
+    std::optional<WrappingInt32> _init_seqno;
+    std::optional<WrappingInt32> _next_ackno;
+    bool _syn_received;
+    bool _fin_received;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity{capacity}, _init_seqno{}, _next_ackno{}, _syn_received{false}, _fin_received{false} {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
