30aab1d48a44f2cf62e115782f510a0e3fb6275c
xuhuidong <HuiD_Xu@163.com>
2022-06-09 18:46:55 -0700

    finish lab 1 coding

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -33,15 +33,16 @@ void StreamReassembler::discard_substrings() {
 }
 
 void StreamReassembler::merge_substrings() {
-
     auto iter_prev = _substrings.begin(), iter_next = iter_prev;
-    if (iter_next != _substrings.end()) ++iter_next;
-    
+    if (iter_next != _substrings.end())
+        ++iter_next;
+
     while (iter_next != _substrings.end()) {
         uint64_t prev_end = iter_prev->index + iter_prev->data.size();
         uint64_t next_begin = iter_next->index, next_end = iter_next->index + iter_next->data.size();
         // prev_begin <= next_begin < next_end <= prev_end
-        if (next_end <= prev_end) iter_next = _substrings.erase(iter_next);
+        if (next_end <= prev_end)
+            iter_next = _substrings.erase(iter_next);
         else if (next_begin <= prev_end) {
             // prev_begin <= next_begin <= prev_end < next_end
             iter_prev->data.append(iter_next->data.substr(prev_end - next_begin));
@@ -59,13 +60,15 @@ void StreamReassembler::write_substrings() {
     while (!_substrings.empty()) {
         auto iter = _substrings.begin();
         size_t idx_begin = iter->index, idx_end = iter->index + iter->data.size();
-        cout << "write_substrings:  idx_begin: " << idx_begin << " idx_end: " << idx_end << "\n";
-        if (_nextbyte >= idx_end) _substrings.erase(iter);
-        else if (_nextbyte < idx_begin) break;
-        else { 
-            // just regard the piece as a incoming part and reuse the function to handle it
+        if (_nextbyte >= idx_end)
             _substrings.erase(iter);
-            push_substring(iter->data, iter->index, iter->eof);
+        else if (_nextbyte < idx_begin)
+            break;
+        else {
+            // just regard the piece as a incoming part and reuse the function to handle it
+            const size_t writebytes = _output.write(iter->data.substr(_nextbyte - idx_begin));
+            _nextbyte += writebytes;
+            _substrings.erase(iter);
         };
     };
 }
@@ -82,20 +85,21 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
 
     // already have read it or store it in the ByteStream
     // simply discard it
-    if (_nextbyte >= index_end) {}
-    else if (index_begin <= _nextbyte && _nextbyte < index_end) {
+    if (_nextbyte >= index_end) {
+    } else if (index_begin <= _nextbyte && _nextbyte < index_end) {
         // directly write it into the ByteStream as much as we can
         const size_t writebytes = _output.write(data.substr(_nextbyte - index_begin));
         _nextbyte += writebytes;
 
         write_substrings();
-    }
-    else if (_nextbyte < index_begin) {
-        // insert and combine the part into the _substrings
+    } else if (_nextbyte < index_begin) {
+        // insert and combine the part into the _substrings, find the correct position to insert
         auto iter = _substrings.begin();
         while (iter != _substrings.end()) {
-            if (iter->index + iter->data.size() >= index_begin) break;
-            else continue;
+            if (iter->index >= index_begin)
+                break;
+            else
+                iter++;
         };
 
         _substrings.insert(iter, substring(data, index, eof));
@@ -103,10 +107,8 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
 
     merge_substrings();
     discard_substrings();
-    cout << "_eof: " << _eof << " _next_bytes: " << _nextbyte << "_end_index: " << _end_index << "\n";
     if (empty())
         _output.end_input();
-    // cout << "_output bytestream size:  " << _output.buffer_size() << "\n";
     return;
 }
 
@@ -118,6 +120,4 @@ size_t StreamReassembler::unassembled_bytes() const {
     return cnt;
 }
 
-bool StreamReassembler::empty() const {
-    return _eof && _nextbyte >= _end_index;
-}
+bool StreamReassembler::empty() const { return _eof && _nextbyte >= _end_index; }
